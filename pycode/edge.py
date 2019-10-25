91c1537b98974ddbbecf9464b1af53adf2a4a3ac
shoda888 <gekka888@gmail.com>
2019-10-25 14:25:29 +0900

    新規ファイル追加

--- pycode/edge.py
+++ pycode/edge.py
@@ -5,24 +5,24 @@ ORG_WINDOW_NAME = "org"
 GRAY_WINDOW_NAME = "gray"
 CANNY_WINDOW_NAME = "canny"
 
-ORG_FILE_NAME = "depth.png"
-GRAY_FILE_NAME = "gray.png"
-CANNY_FILE_NAME = "depth_canny.png"
+ORG_FILE_NAME = "rgb.jpg"
+GRAY_FILE_NAME = "gray.jpg"
+CANNY_FILE_NAME = "rgb_canny.png"
 
 # 元の画像を読み込む
 org_img = cv2.imread(ORG_FILE_NAME, cv2.IMREAD_UNCHANGED)
 # グレースケールに変換
 gray_img = cv2.imread(ORG_FILE_NAME, cv2.IMREAD_GRAYSCALE)
 # エッジ抽出
-canny_img = cv2.Canny(gray_img, 100, 250)
+canny_img = cv2.Canny(gray_img, 250, 300)
 
 # ウィンドウに表示
 cv2.namedWindow(ORG_WINDOW_NAME)
 cv2.namedWindow(GRAY_WINDOW_NAME)
 cv2.namedWindow(CANNY_WINDOW_NAME)
 
-cv2.imshow(ORG_WINDOW_NAME, org_img)
-cv2.imshow(GRAY_WINDOW_NAME, gray_img)
+# cv2.imshow(ORG_WINDOW_NAME, org_img)
+# cv2.imshow(GRAY_WINDOW_NAME, gray_img)
 cv2.imshow(CANNY_WINDOW_NAME, canny_img)
 
 # ファイルに保存
