9b8f8de7947a469f40ec8851b3becb87e1a00527
shoda888 <gekka888@gmail.com>
2020-03-14 18:54:35 +0900

    時系列データとメディアンフィルタでのdepth欠損補完の実装

---
+++ matlabcode/repair.m
@@ -0,0 +1,30 @@
+depth = imageDatastore(fullfile('depth'),...
+'IncludeSubfolders',true,'FileExtensions','.png','LabelSource','foldernames');
+imgs = readall(depth);
+framenum = length(imgs);
+height = 288;
+width = 320;
+mat3d = zeros(height, width, framenum);
+itr = 100;
+
+for i = 1:framenum
+    mat3d(:,:,i) = imgs{i};
+end
+
+mat3d(mat3d == 0) = nan; %欠損値をNaNに置換
+mat3d(mat3d > 8400) = nan; %測定範囲外をNaNに置換
+output = median(mat3d,3,'omitnan');
+output(isnan(output)) = 0;
+imwrite(imadjust(uint16(output)),'output1.png', 'BitDepth',16); %時系列データで補完したdepthマップ
+
+
+for j = 1:itr
+    m = 2 * round(j / 10) + 3;
+    filtered = medfilt2(output, [m,m]);
+    tmp = output;
+    output(output == 0) = filtered(output == 0);
+    if isequal(output, tmp)
+        break
+    end
+    imwrite(imadjust(uint16(output)),append('output',int2str(j+1),'.png'), 'BitDepth',16);
+end
--- pycode/bitshifting.py
+++ pycode/bitshifting.py
@@ -1,8 +1,13 @@
 from PIL import Image
 import numpy as np
+import glob
 
-im = np.array(Image.open('pycode/0000000001.tiff'))
-im_f = im * 65535.0 / 8000
+# l = glob.glob("depth/*.png")
+l = glob.glob("depth_repaired/*.png")
 
-pil_img = Image.fromarray(im_f.astype(np.uint16))
-pil_img.save('pycode/0000000001a.png')
+for i,name in enumerate(l):
+    im = np.array(Image.open(name))
+    im_f = im * 65535.0 / 8000
+    pil_img = Image.fromarray(im_f.astype(np.uint16))
+    # pil_img.save(f'depth_shifted/depth_shifted{i+1}.png')
+    pil_img.save(name)
