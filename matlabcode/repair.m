f96c9c5a77bbe1f40919223ae5c76b3bc01290c1
shoda888 <gekka888@gmail.com>
2020-03-29 16:29:17 +0900

    fix matlab code

--- matlabcode/repair.m
+++ matlabcode/repair.m
@@ -1,11 +1,11 @@
-depth = imageDatastore(fullfile('depth'),...
-'IncludeSubfolders',true,'FileExtensions','.png','LabelSource','foldernames');
+depth = imageDatastore(fullfile('sample'),...
+'IncludeSubfolders',true,'FileExtensions','.tiff','LabelSource','foldernames');
 imgs = readall(depth);
 framenum = length(imgs);
-height = 288;
-width = 320;
+height = 424;
+width = 512;
 mat3d = zeros(height, width, framenum);
-itr = 100;
+itr = 20;
 
 for i = 1:framenum
     mat3d(:,:,i) = imgs{i};
@@ -15,7 +15,7 @@ mat3d(mat3d == 0) = nan; %欠損値をNaNに置換
 mat3d(mat3d > 8400) = nan; %測定範囲外をNaNに置換
 output = median(mat3d,3,'omitnan');
 output(isnan(output)) = 0;
-imwrite(imadjust(uint16(output)),'output1.png', 'BitDepth',16); %時系列データで補完したdepthマップ
+imwrite(imadjust(uint16(output)),'sample/output1.png', 'BitDepth',16); %時系列データで補完したdepthマップ
 
 
 for j = 1:itr
@@ -26,5 +26,5 @@ for j = 1:itr
     if isequal(output, tmp)
         break
     end
-    imwrite(imadjust(uint16(output)),append('depth_repaired/output',int2str(j+1),'.png'), 'BitDepth',16);
+    imwrite(imadjust(uint16(output)),append('sample/output',int2str(j+1),'.png'), 'BitDepth',16);
 end
